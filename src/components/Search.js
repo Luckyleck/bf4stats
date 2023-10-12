14e0d36653b4ebe8466e814a114882fdac350ca6
Alex <luckyleck29@gmail.com>
2023-10-12 15:17:29 -0400

    Adding fade effect on search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'
-import './searchStyles.css'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './searchStyles.css';
 
 export default function Search() {
-    const [playerName, setPlayerName] = useState('')
+    const [playerName, setPlayerName] = useState('');
+    const [searching, setSearching] = useState(false); // Add a state to track the searching status
     const navigate = useNavigate();
 
     const handleSearch = () => {
-        navigate(`/stats/${playerName}`)
+        setSearching(true); // Set searching to true to trigger the animation
+        // Delay navigation to allow the animation to play
+        setTimeout(() => {
+            navigate(`/stats/${playerName}`);
+        }, 800); // Adjust the delay time as needed
     }
 
     const handleKeyPress = (e) => {
-        if (e.key === "Enter") {
+        if (e.key === 'Enter') {
             handleSearch();
         }
     }
 
     return (
-        <div className="search-main">
+        <div className={`search-main ${searching ? 'fade-out' : ''}`}>
             <h1 className="header">Battlefield 4 Stats</h1>
             <div className="search-bar">
                 <input
@@ -31,5 +35,5 @@ export default function Search() {
                 <button onClick={handleSearch}>Search</button>
             </div>
         </div>
-    )
+    );
 }
